dd4583c2ec8fb59f5f4472b6df2c7a94be5cef1b
Nicholas Ingolia <nick@ingolia.org>
2015-08-04 21:35:10 -0700

    Read serial data using software serial while still monitoring the PPS using an interrupt

--- trinket-interrupt/trinket-interrupt.ino
+++ trinket-interrupt/trinket-interrupt.ino
@@ -1,10 +1,21 @@
+#include <SoftwareSerial.h>
+
 #include <avr/interrupt.h>
+
+#define RXPIN 0
+#define TXPIN 3
+#define RATE 9600
+
+SoftwareSerial serialGPS = SoftwareSerial(RXPIN, TXPIN);
 
 volatile unsigned long lastPulseMs;
 unsigned int cycle = 0;
 
 void setup() {
   // put your setup code here, to run once:
+
+  pinMode(RXPIN, INPUT);
+  pinMode(TXPIN, OUTPUT);
 
   pinMode(1, OUTPUT);
   pinMode(2, INPUT);
@@ -13,6 +24,8 @@ void setup() {
   lastPulseMs = 0;
 
   attachInterrupt(0, ppsIsr, RISING);
+
+  serialGPS.begin(RATE);
 }
 
 void loop() {
@@ -20,11 +33,16 @@ void loop() {
 
   unsigned long now = millis();
 
-  digitalWrite(4, (now < lastPulseMs + 250) ? HIGH : LOW);
-  digitalWrite(1, ((cycle % 256) > 127) ? HIGH : LOW);
-  cycle++;
+  digitalWrite(1, (now < lastPulseMs + 250) ? HIGH : LOW);
 
-  delay(1);
+  while (serialGPS.available()) {
+//    digitalWrite(4, HIGH);
+    char c = serialGPS.read();
+    analogWrite(4, c);
+  }
+  digitalWrite(4, LOW);
+  
+  cycle++;
 }
 
 void ppsIsr(void)
